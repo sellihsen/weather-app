3db0912aa83f180895f7a50016e1e882435a8601
Ihsen SELLAOUTI <isellaouti@astrelya.com>
2025-11-04 11:33:04 +0100

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,9 @@
+variable "resource_group_name_prefix" {
+  description = "Prefix for resource group names"
+  type        = string
+  default     = "rg"   # Set a sensible default or leave empty
+}
+
 variable "resource_group_name" {
   default = "rg-weather"
 }
