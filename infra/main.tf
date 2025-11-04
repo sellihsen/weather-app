63b2d73e58f315a1e5041c83f519310a222c9c6a
Ihsen SELLAOUTI <isellaouti@astrelya.com>
2025-11-04 15:59:58 +0100

    Update infra workflow

--- infra/main.tf
+++ infra/main.tf
@@ -29,11 +29,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   sku_tier = "Free"
 
-  azure_active_directory {
-    managed                = true
-    admin_group_object_ids = []
-  }
-
   depends_on = [azurerm_container_registry.acr]
 }
 
